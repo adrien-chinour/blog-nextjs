77ce85a7793d75fe24908f1a21e64ff780a9fc13
Adrien Chinour <github@chinour.fr>
2024-09-21 11:10:46 +0200

    feat: add ask openai component

--- src/components/article/ArticleContent.tsx
+++ src/components/article/ArticleContent.tsx
@@ -1,9 +1,12 @@
 import {Article} from "@/types/models";
+import AskOpenAi from "@/components/openai/AskOpenAi";
 
 export default function ArticleContent({article}: {article: Article}) {
     const html = {__html: article.content};
 
     return (
-        <div id="article-content" className="container-fit" dangerouslySetInnerHTML={html} />
+        <AskOpenAi>
+            <div id="article-content" className="container-fit" dangerouslySetInnerHTML={html}/>
+        </AskOpenAi>
     )
 }
