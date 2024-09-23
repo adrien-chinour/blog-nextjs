ccb9a54d8b1e7e625b55f6e27c706bc2d131f3d0
Adrien Chinour <github@chinour.fr>
2024-09-23 18:42:43 +0200

    fix: remove dependencies on useEffect

--- src/components/HistoryLogger.tsx
+++ src/components/HistoryLogger.tsx
@@ -1,14 +1,15 @@
 "use client"
 
-import {useEffect} from "react";
 import {useHistory} from "@/contexts/history-context";
+import {useEffect} from "react";
 
 export default function HistoryLogger({title, href}: Readonly<{ title: string, href: string }>) {
     const {pushItem} = useHistory();
 
     useEffect(() => {
+        console.log("Push item on History Logger")
         pushItem({type: 'article', title: title, href: href});
-    }, [title, href, pushItem])
+    }, []);
 
     return (
         <></>
