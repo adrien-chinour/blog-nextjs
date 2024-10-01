26b2712ab9faf850bb3b9bc766262102db99b557
Adrien Chinour <github@chinour.fr>
2024-10-01 23:55:37 +0200

    fix: mobile screen issue

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer role="contentinfo"
             className="flex justify-between py-6 mt-2 items-center uppercase text-zinc-600 dark:text-zinc-400 text-xs font-bold px-4">
-            <p>© Adrien Chinour</p>
+            <p className="hidden lg:block">© Adrien Chinour</p>
             <ul className="flex gap-4">
                 <li>
                     <a className="hover:underline hover:text-zinc-700 hover:dark:text-zinc-300" target="_blank"
