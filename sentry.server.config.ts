6a00a137390c73e696ab2f8567f8c2b2142a7272
Adrien Chinour <github@chinour.fr>
2024-10-12 17:35:24 +0200

    chore: disable replay on sentry and add sampling on client tracing

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,10 +6,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://5f7858103cada86a3ddf8902a928728e@o1133634.ingest.us.sentry.io/4508108863700992",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
