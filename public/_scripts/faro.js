5831f025e1dd47340ef9f0bc7930112ca2b81c22
Adrien Chinour <github@chinour.fr>
2024-10-12 17:41:35 +0200

    fix: faro script not loaded

--- public/_scripts/faro.js
+++ public/_scripts/faro.js
@@ -1,13 +1,11 @@
 (function () {
-    window.addEventListener('DOMContentLoaded', () => {
-        const webSdkScript = document.createElement("script");
-        webSdkScript.src = "https://unpkg.com/@grafana/faro-web-sdk@^1.4.0/dist/bundle/faro-web-sdk.iife.js";
-        webSdkScript.onload = () => {
-            window.GrafanaFaroWebSdk.initializeFaro({
-                url: "https://faro-collector-prod-eu-west-0.grafana.net/collect/8f64d55ee7070b3ec03abe36e2a917df",
-                app: {name: "next.udfn.fr", version: "1.0.0", environment: "production"},
-            });
-        };
-        document.body.appendChild(webSdkScript);
-    });
+    const webSdkScript = document.createElement("script");
+    webSdkScript.src = "https://unpkg.com/@grafana/faro-web-sdk@^1.4.0/dist/bundle/faro-web-sdk.iife.js";
+    webSdkScript.onload = () => {
+        window.GrafanaFaroWebSdk.initializeFaro({
+            url: "https://faro-collector-prod-eu-west-0.grafana.net/collect/8f64d55ee7070b3ec03abe36e2a917df",
+            app: {name: "next.udfn.fr", version: "1.0.0", environment: "production"},
+        });
+    };
+    document.body.appendChild(webSdkScript);
 })();
