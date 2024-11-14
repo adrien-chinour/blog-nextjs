1947515d58267e9f04b725f62ec730a334a78b9a
Adrien Chinour <github@chinour.fr>
2024-11-14 22:31:21 +0100

    fix: change faro collector endpoint

--- public/_scripts/faro.js
+++ public/_scripts/faro.js
@@ -3,8 +3,8 @@
     webSdkScript.src = "https://unpkg.com/@grafana/faro-web-sdk@^1.4.0/dist/bundle/faro-web-sdk.iife.js";
     webSdkScript.onload = () => {
         window.GrafanaFaroWebSdk.initializeFaro({
-            url: "https://faro-collector-prod-eu-west-0.grafana.net/collect/8f64d55ee7070b3ec03abe36e2a917df",
-            app: {name: "next.udfn.fr", version: "1.0.0", environment: "production"},
+            url: "https://faro-collector-prod-eu-west-0.grafana.net/collect/9689c3ba5a20d52b36dec6a5da24f8eb",
+            app: {name: "udfn.fr", version: "1.0.0", environment: "production"},
         });
     };
     document.head.appendChild(webSdkScript);
