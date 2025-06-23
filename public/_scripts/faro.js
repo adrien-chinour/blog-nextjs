3f249a55aed51c0803dadb3ac6bcfc61ffba08c9
Adrien Chinour <github@chinour.fr>
2025-06-23 22:23:43 +0200

    fix: set faro sdk version

--- public/_scripts/faro.js
+++ public/_scripts/faro.js
@@ -1,6 +1,6 @@
 (function () {
     const webSdkScript = document.createElement("script");
-    webSdkScript.src = "https://unpkg.com/@grafana/faro-web-sdk@^1.4.0/dist/bundle/faro-web-sdk.iife.js";
+    webSdkScript.src = "https://unpkg.com/@grafana/faro-web-sdk@1.18.2/dist/bundle/faro-web-sdk.iife.js";
     webSdkScript.onload = () => {
         window.GrafanaFaroWebSdk.initializeFaro({
             url: "https://faro-collector-prod-eu-west-0.grafana.net/collect/9689c3ba5a20d52b36dec6a5da24f8eb",
