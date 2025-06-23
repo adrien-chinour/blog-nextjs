241a9b55cc24514d4b3391df1f5d0dd86efdb06e
Adrien Chinour <github@chinour.fr>
2025-06-23 21:51:21 +0200

    feat: add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,14 @@ const nextConfig = {
                         key: 'Cache-Control',
                         value: 'public, max-age=60, s-maxage=3600, stale-while-revalidate=86400',
                     },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY'
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload'
+                    }
                 ],
             },
         ];
