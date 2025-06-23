42b74a653e26377f101fc5ec37b7e3a5d6fe7791
Adrien Chinour <github@chinour.fr>
2025-06-23 21:47:57 +0200

    feat: add cache control on routes

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,20 @@ const nextConfig = {
                 pathname: '/**',
             },
         ],
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=60, s-maxage=3600, stale-while-revalidate=86400',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
