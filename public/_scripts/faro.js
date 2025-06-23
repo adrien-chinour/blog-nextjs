2ceeb3bc188e28e7c9f55fa4abf6c022ccf949f4
Adrien Chinour <github@chinour.fr>
2025-06-23 23:44:47 +0200

    perf: defer faro after page load

--- public/_scripts/faro.js
+++ public/_scripts/faro.js
@@ -1,12 +1,16 @@
-const webSdkScript = document.createElement("script");
-webSdkScript.src = "https://unpkg.com/@grafana/faro-web-sdk@1.18.2/dist/bundle/faro-web-sdk.iife.js";
-webSdkScript.onload = () => {
+const faroInitailization = () => {
     window.GrafanaFaroWebSdk.initializeFaro({
         url: "https://faro-collector-prod-eu-west-0.grafana.net/collect/9689c3ba5a20d52b36dec6a5da24f8eb",
         app: {name: "udfn.fr", version: "1.0.0", environment: "production"},
     });
-};
-webSdkScript.async = true;
-webSdkScript.fetchpriority = "low";
+}
 
-document.body.appendChild(webSdkScript);
+
+window.addEventListener('load', () => {
+    const webSdkScript = document.createElement("script");
+    webSdkScript.src = "https://unpkg.com/@grafana/faro-web-sdk@1.18.2/dist/bundle/faro-web-sdk.iife.js";
+    webSdkScript.onload = faroInitailization;
+    webSdkScript.async = true;
+
+    document.body.appendChild(webSdkScript);
+})
